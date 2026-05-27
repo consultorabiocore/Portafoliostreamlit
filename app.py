5e91cd4675ec8950ad18fc8887d3ce0e16362fff
consultorabiocore <consultorabiocore@gmail.com>
2026-05-27 15:44:49 -0400

    Update app.py

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import streamlit as st
 
 # Configuración de la página
 st.set_page_config(
-    page_title="Tu Nombre | Portafolio EnviroTech",
+    page_title="Loreto Campos Carrasco | Portafolio EnviroTech",
     page_icon="🌿",
     layout="centered"
 )
